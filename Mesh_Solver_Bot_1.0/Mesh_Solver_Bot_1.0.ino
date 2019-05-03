ee8fa1ec48fa4f39272b89b4a10363396e7bd581
aviralpathak <devaviral03@gmail.com>
2019-05-03 17:53:34 +0530

    Upload mesh solver robot code
    Signed-off-by: aviralpathak <devaviral03@gmail.com>

---
+++ Mesh_Solver_Bot_1.0/Mesh_Solver_Bot_1.0.ino
@@ -0,0 +1,378 @@
+#define leftFarSensor   3
+#define leftNearSensor  4     
+#define rightNearSensor 5
+#define rightFarSensor  6
+
+int leftFarReading;
+int leftNearReading;
+int rightNearReading;
+int rightFarReading;
+
+
+int leftNudge;
+int replaystage;
+int rightNudge;
+
+#define leapTime 500
+
+#define leftMotor1  10   //brown
+#define leftMotor2  9 //yellow
+
+#define rightMotor1 8  //grey
+#define rightMotor2 11  //red
+
+
+#define led 13
+
+char path[30] = {};
+int pathLength;
+int readLength;
+
+void setup(){
+  
+  pinMode(leftNearSensor, INPUT);
+  pinMode(leftFarSensor, INPUT);
+  pinMode(rightNearSensor, INPUT);
+  pinMode(rightFarSensor, INPUT);
+    
+  pinMode(leftMotor1, OUTPUT);
+  pinMode(leftMotor2, OUTPUT);
+  pinMode(rightMotor1, OUTPUT);
+  pinMode(rightMotor2, OUTPUT);
+  pinMode(led, OUTPUT);
+  
+}
+
+
+void loop(){          // 0-->black .    1-->white
+  
+ readSensors();                                                                                     
+ 
+ if(leftFarReading==0 && rightFarReading==0)&&(leftNearReading==1||rightNearReading==1) ){ 
+    straight();                                                                                      
+  }
+  
+
+}
+
+void readSensors(){
+  
+  
+  leftNearReading    = digitalRead(leftNearSensor);
+  leftFarReading     = digitalRead(leftFarSensor);
+  
+  rightNearReading   = digitalRead(rightNearSensor);
+  rightFarReading    = digitalRead(rightFarSensor);  
+
+// serial printing below for debugging purposes
+
+
+// Serial.print("leftNearReading: ");
+// Serial.println(leftNearReading);
+// Serial.print("leftFarReading: ");
+// Serial.println(leftFarReading);
+  
+// Serial.print("rightNearReading: ");
+// Serial.println(rightNearReading);
+// Serial.print("rightFarReading: ");
+// Serial.println(rightFarReading);
+// delay(200);
+  
+
+}
+
+
+
+
+void turnLeft(){
+  
+    
+ 
+  while(digitalRead(rightFarSensor)==1||digitalRead(leftFarSensor)==1){
+     digitalWrite(leftMotor1, LOW);
+    digitalWrite(leftMotor2, HIGH);
+    digitalWrite(rightMotor1, HIGH);
+    digitalWrite(rightMotor2, LOW);
+    delay(3);
+     digitalWrite(leftMotor1, LOW);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, LOW);
+    digitalWrite(rightMotor2, LOW);
+    delay(1);
+  }
+    
+  while(digitalRead(rightCenterSensor)==1){
+    digitalWrite(leftMotor1, LOW);
+    digitalWrite(leftMotor2, HIGH);
+    digitalWrite(rightMotor1, HIGH);
+    digitalWrite(rightMotor2, LOW);
+    delay(3);
+    digitalWrite(leftMotor1, LOW);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, LOW);
+    digitalWrite(rightMotor2, LOW);
+    delay(1);
+  }
+ 
+  if(replaystage==0){
+    path[pathLength]='L';
+    //Serial.println("l");
+    pathLength++;
+    //Serial.print("Path length: ");
+    //Serial.println(pathLength);
+      if(path[pathLength-2]=='B'){
+        //Serial.println("shortening path");
+        shortPath();
+      }
+  }
+}
+
+void turnRight(){
+
+    
+  while(digitalRead(rightCenterSensor)==0){
+     digitalWrite(leftMotor1, HIGH);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, LOW);
+    digitalWrite(rightMotor2, HIGH);
+    delay(3);
+     digitalWrite(leftMotor1, LOW);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, LOW);
+    digitalWrite(rightMotor2, LOW);
+    delay(1);
+  }
+   while(digitalRead(rightCenterSensor)==1){
+     digitalWrite(leftMotor1, HIGH);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, LOW);
+    digitalWrite(rightMotor2, HIGH);
+    delay(3);
+     digitalWrite(leftMotor1, LOW);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, LOW);
+    digitalWrite(rightMotor2, LOW);
+    delay(1);
+  }
+   while(digitalRead(leftCenterSensor)==1){
+     digitalWrite(leftMotor1, HIGH);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, LOW);
+    digitalWrite(rightMotor2, HIGH);
+    delay(3);
+    digitalWrite(leftMotor1, LOW);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, LOW);
+    digitalWrite(rightMotor2, LOW);
+    delay(1);
+  }
+  
+  if(replaystage==0){
+  path[pathLength]='R';
+  Serial.println("r");
+  pathLength++;
+  Serial.print("Path length: ");
+  Serial.println(pathLength);
+    if(path[pathLength-2]=='B'){
+      Serial.println("shortening path");
+      shortPath();
+    }
+  }
+ 
+}
+
+void straight(){
+  if( digitalRead(leftCenterSensor)==1){
+    digitalWrite(leftMotor1, HIGH);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, HIGH);
+    digitalWrite(rightMotor2, LOW);
+    delay(1);
+    digitalWrite(leftMotor1, HIGH);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, LOW);
+    digitalWrite(rightMotor2, LOW);
+    delay(5);
+    return;
+  }
+  if(digitalRead(rightCenterSensor)==1){
+    digitalWrite(leftMotor1, HIGH);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, HIGH);
+    digitalWrite(rightMotor2, LOW);
+    delay(1);
+    digitalWrite(leftMotor1, LOW);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, HIGH);
+    digitalWrite(rightMotor2, LOW);
+    delay(5);
+    return;
+  }
+  
+    digitalWrite(leftMotor1, HIGH);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, HIGH);
+    digitalWrite(rightMotor2, LOW);
+    delay(4);
+    digitalWrite(leftMotor1, LOW);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, LOW);
+    digitalWrite(rightMotor2, LOW);
+    delay(1);
+  
+}
+
+void turnAround(){
+    digitalWrite(leftMotor1, HIGH);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, HIGH);
+    digitalWrite(rightMotor2, LOW);
+    delay(150);
+   while(digitalRead(rightCenterSensor)==1){
+     digitalWrite(leftMotor1, LOW);
+    digitalWrite(leftMotor2, HIGH);
+    digitalWrite(rightMotor1, HIGH);
+    digitalWrite(rightMotor2, LOW);
+    delay(3);
+     digitalWrite(leftMotor1, LOW);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, LOW);
+    digitalWrite(rightMotor2, LOW);
+    delay(1);
+  }
+  path[pathLength]='B';
+  pathLength++;
+  straight();
+  //Serial.println("b");
+  //Serial.print("Path length: ");
+  //Serial.println(pathLength);
+}
+
+void shortPath(){
+ int shortDone=0;
+  if(path[pathLength-3]=='L' && path[pathLength-1]=='R'){
+    pathLength-=3;
+    path[pathLength]='B';
+    //Serial.println("test1");
+    shortDone=1;
+  }
+   
+  if(path[pathLength-3]=='L' && path[pathLength-1]=='S' && shortDone==0){
+    pathLength-=3;
+    path[pathLength]='R';
+    //Serial.println("test2");
+    shortDone=1;
+  }
+   
+  if(path[pathLength-3]=='R' && path[pathLength-1]=='L' && shortDone==0){
+    pathLength-=3;
+    path[pathLength]='B';
+    //Serial.println("test3");
+    shortDone=1;
+  }
+  
+   
+  if(path[pathLength-3]=='S' && path[pathLength-1]=='L' && shortDone==0){
+    pathLength-=3;
+    path[pathLength]='R';
+    //Serial.println("test4");
+    shortDone=1;
+  }
+     
+  if(path[pathLength-3]=='S' && path[pathLength-1]=='S' && shortDone==0){
+    pathLength-=3;
+    path[pathLength]='B';
+    //Serial.println("test5");
+    shortDone=1;
+  }
+    if(path[pathLength-3]=='L' && path[pathLength-1]=='L' && shortDone==0){
+    pathLength-=3;
+    path[pathLength]='S';
+    //Serial.println("test6");
+    shortDone=1;
+  }
+  
+  path[pathLength+1]='D';
+  path[pathLength+2]='D';
+  pathLength++;
+  //Serial.print("Path length: ");
+  //Serial.println(pathLength);
+  //printPath();
+}
+
+
+
+
+void printPath(){
+  Serial.println("+++++++++++++++++");
+  int x;
+  while(x<=pathLength){
+  Serial.println(path[x]);
+  x++;
+  }
+  Serial.println("+++++++++++++++++");
+}
+
+
+void replay(){
+   readSensors();
+  if(leftFarReading==1 && rightFarReading==1){
+    straight();
+  }
+  else{
+    if(path[readLength]=='D'){
+       digitalWrite(leftMotor1, HIGH);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, HIGH);
+    digitalWrite(rightMotor2, LOW);
+    delay(100);
+    digitalWrite(leftMotor1, LOW);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, LOW);
+    digitalWrite(rightMotor2, LOW);
+      endMotion();
+    }
+    if(path[readLength]=='L'){
+     digitalWrite(leftMotor1, HIGH);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, HIGH);
+    digitalWrite(rightMotor2, LOW);
+    delay(leapTime);
+      turnLeft();
+    }
+    if(path[readLength]=='R'){
+       digitalWrite(leftMotor1, HIGH);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, HIGH);
+    digitalWrite(rightMotor2, LOW);
+    delay(leapTime);
+    turnRight();
+    }
+    if(path[readLength]=='S'){
+       digitalWrite(leftMotor1, HIGH);
+    digitalWrite(leftMotor2, LOW);
+    digitalWrite(rightMotor1, HIGH);
+    digitalWrite(rightMotor2, LOW);
+    delay(leapTime);
+    straight();
+    }
+    
+    readLength++;
+  }
+    
+  replay();
+  
+}
+
+void endMotion(){
+    digitalWrite(led, LOW);
+    delay(500);
+    digitalWrite(led, HIGH);
+    delay(200);
+      digitalWrite(led, LOW);
+    delay(200);
+    digitalWrite(led, HIGH);
+    delay(500);
+  endMotion();
+}
+
